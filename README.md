11367ce416a7b780abab6053b67bc7a915c2a582
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2016-12-15 10:34:32 -0800

    Some people miss the message because they are too busy looking for a mistake.

---
+++ README.md
@@ -0,0 +1,8 @@
+A plugin to replace https://web.archive.org/web/20161215183203/https://www.spigotmc.org/resources/gender.26120/
+
+Images:
+http://i.imgur.com/SfgWJQJ.png
+http://i.imgur.com/IRg0ZLj.png
+http://i.imgur.com/p3ItAgB.png
+http://i.imgur.com/KsUjhxf.png
+http://i.imgur.com/SOYZeUP.png
---
+++ src/main/java/net/poweredbyhate/gender/Gender.java
@@ -0,0 +1,83 @@
+package net.poweredbyhate.gender;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+/**
+ * Created by Lax on 12/15/2016.
+ */
+public class Gender extends JavaPlugin {
+
+
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+
+    public InputStream getGenderFile() {
+        return getClassLoader().getResourceAsStream("genders.yml");
+    }
+
+    public FileConfiguration getGenderConfig() {
+        try {
+            FileConfiguration genderConfig = new YamlConfiguration();
+            genderConfig.load(getGenderFile());
+            return genderConfig;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Set<String> getGenders() {
+        return getGenderConfig().getKeys(false);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("gender.gender")) {
+            sender.sendMessage(ChatColor.RED + "You are not allowed to have a gender.");
+            return true;
+        }
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("list")) {
+                sender.sendMessage(ChatColor.AQUA+"("+getGenders().size()+") " + java.util.Arrays.toString(getGenders().toArray()));
+            }
+        }
+        if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("info")) {
+                if (getGenderConfig().getString(args[1]+".description") != null) {
+                    sender.sendMessage(ChatColor.GREEN + args[1] + ": " + ChatColor.AQUA + getGenderConfig().getString(args[1]+".description"));
+                } else {
+                    sender.sendMessage("Such gender does not exist yet.");
+                }
+            }
+            if (args[0].equalsIgnoreCase("set") && getGenderConfig().getString(args[1]+".description") != null && sender instanceof Player) {
+                getConfig().set(((Player) sender).getUniqueId().toString(), args[1]);
+                saveConfig();
+                sender.sendMessage(ChatColor.GREEN + "You now identify as " + ChatColor.AQUA + args[1]);
+            }
+            if (args[0].equalsIgnoreCase("check")) {
+                if (getConfig().getString(Bukkit.getOfflinePlayer(args[1]).getUniqueId().toString()) == null || !Bukkit.getOfflinePlayer(args[1]).hasPlayedBefore()) {
+                    sender.sendMessage("The player you have dialed is not yet in service");
+                } else {
+                    sender.sendMessage(ChatColor.GREEN + args[1]+" identify as " + ChatColor.AQUA + getConfig().getString(Bukkit.getOfflinePlayer(args[1]).getUniqueId().toString()));
+                }
+            }
+        }
+        return false;
+    }
+
+}
